161f0ec5a4eff3417e0f1af0212a5eeba060a4e0
sauravvishal8797 <sauravvishal8797@gmail.com>
2018-11-05 03:44:12 +0530

    Added layouts for main page and about page

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title="About Page")
 
 
 if __name__ == '__main__':
