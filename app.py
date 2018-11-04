a576a88802a3cecde42434fbc193edf6708cfb51
sauravvishal8797 <sauravvishal8797@gmail.com>
2018-11-05 01:41:34 +0530

    Added dummy posts for testing

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+posts = [
+    {
+        'Author': 'Saurav Vishal',
+        'Title': 'My journey with Flask',
+        'Content': 'This is a dummy blog post intended for checking the working of the ap'
+                   'p'
+    },
+    {
+        'Author': 'Raj Malhorta',
+        'Title': 'Has to be someone',
+        'Content': 'Ploppy is the new San Fernandino to my state of Kansas'
+    },
+    {
+        'Author': 'Sakuni Jajab',
+        'Title': 'The take of the Mahabharata',
+        'Content': 'The war of the Mahabharata has a special significance '
+                   'in the mythological history of Republic of India'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
 def index():
-    return "Hello World"
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
