7a259d85080fa4fafa4b2c8e9c7115ce7b136f1f
sauravvishal8797 <sauravvishal8797@gmail.com>
2018-11-11 22:45:06 +0530

    Modified login and register layout pages

--- app.py
+++ app.py
@@ -1,48 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
-app.config['SECRET KEY'] = ''
-
+app.config['SECRET_KEY'] = 'd74306e39550f79330dbd2e530d4beae'
 posts = [
-    {
-        'Author': 'Saurav Vishal',
-        'Title': 'My journey with Flask',
-        'Content': 'This is a dummy blog post intended for checking the working of the ap'
-                   'p'
-    },
-    {
-        'Author': 'Raj Malhorta',
-        'Title': 'Has to be someone',
-        'Content': 'Ploppy is the new San Fernandino to my state of Kansas'
-    },
-    {
-        'Author': 'Sakuni Jajab',
-        'Title': 'The take of the Mahabharata',
-        'Content': 'The war of the Mahabharata has a special significance '
-                   'in the mythological history of Republic of India'
-    }
+	{
+		'title': 'Blog post 1',
+		'author': 'Saurav Vishal',
+		'date': '21st of July',
+		'content': 'First blog post'
+	},
+	{
+		'title': 'Blog post 2',
+		'author': 'David Malan',
+		'date': '21st of June',
+		'content': 'Second blog post'
+	},
+	{
+		'title': 'Blog post 3',
+		'author': 'David J Malan',
+		'date': '21st of June',
+		'content': '3rd blog post'
+	}
 ]
 
-@app.route("/")
-@app.route("/home")
-def index():
-    return render_template('home.html', posts=posts)
 
-@app.route("/about")
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template('home.html', posts = posts)
+
+@app.route('/about')
 def about():
-    return render_template('about.html', title="About Page")
+	return render_template('about.html')
 
-@app.route("/register")
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
-    return render_template('register.html', title='Register', form=form)
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for user {form.userName.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', form=form, title='Register')
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    return render_template('login.html', title='Login', form=form)
-
+	form = LoginForm()
+	return render_template('login.html', form=form, title="Login Here")
 
 if __name__ == '__main__':
-    app.run(port=9900, debug=True)
+	app.run(port=9900,debug=True)
+
